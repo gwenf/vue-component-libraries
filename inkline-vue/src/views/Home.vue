d4bf2fd57c4314941b1ee7d7a7d714f1780827c6
gwenf <gwenfaraday@gmail.com>
2019-12-30 19:32:14 -0500

    added form in modal

--- inkline-vue/src/views/Home.vue
+++ inkline-vue/src/views/Home.vue
@@ -4,37 +4,48 @@
 
     <div class="hero-text">
       <h1>Adopt a new best friend.</h1>
-      <i-button @click="togglePetForm" variant="primary">
+      <i-button @click="showPetForm = true" variant="primary">
         Add New Pet
       </i-button>
     </div>
 
-    <i-form @submit.prevent="handleSubmit" v-if="showPetForm">
-      <i-form-group id="exampleInputGroup2" label="Pet's Name:" label-for="exampleInput2">
-        <i-form-input
-          id="exampleInput2"
-          type="text"
-          v-model="formData.name"
-          required
-          placeholder="Enter name" />
-      </i-form-group>
+    <i-modal variant="info" v-model="showPetForm">
+      <template slot="header">Pet Information</template>
 
-      <i-form-group id="exampleInputGroup3" label="Species:" label-for="exampleInput3">
-        <i-form-select id="exampleInput3" :options="['cats', 'dogs']" required v-model="formData.species" />
-      </i-form-group>
+      <i-form @submit.prevent="handleSubmit" v-if="showPetForm">
+        <i-form-group id="exampleInputGroup2" label="Pet's Name:" label-for="exampleInput2">
+          <i-input
+            id="exampleInput2"
+            type="text"
+            v-model="formData.name"
+            required
+            placeholder="Enter name"
+          />
+        </i-form-group>
 
-      <i-form-group id="exampleInputGroup2" label="Pet's Age:" label-for="exampleInput2">
-        <i-form-input
-          id="exampleInput2"
-          type="number"
-          v-model="formData.age"
-          required
-          placeholder="Enter age" />
-      </i-form-group>
+        <i-form-group id="exampleInputGroup3" label="Species:" label-for="exampleInput3">
+          <i-select
+            id="exampleInput3"
+            :options="['cats', 'dogs']"
+            required
+            v-model="formData.species"
+          />
+        </i-form-group>
 
-      <i-button type="submit" variant="primary">Submit</i-button>
-      <i-button type="reset" variant="danger">Reset</i-button>
-    </i-form>
+        <i-form-group id="exampleInputGroup2" label="Pet's Age:" label-for="exampleInput2">
+          <i-input
+            id="exampleInput2"
+            type="number"
+            v-model="formData.age"
+            required
+            placeholder="Enter age"
+          />
+        </i-form-group>
+
+        <i-button type="submit" variant="primary">Submit</i-button>
+        <i-button type="reset" variant="danger">Reset</i-button>
+      </i-form>
+    </i-modal>
   </div>
 </template>
 
